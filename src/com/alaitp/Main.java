2616bc0febfa08667218e96e7df3e930051443f7
Junpeng He <hejunpeng2012@hotmail.com>
2020-04-16 20:47:34 +0800

    thread internal exception

--- src/com/alaitp/Main.java
+++ src/com/alaitp/Main.java
@@ -6,16 +6,16 @@ public class Main {
 	    Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("we are now in thread " + Thread.currentThread().getName());
-                System.out.println("Current thread priority is " + Thread.currentThread().getPriority());
+                throw new RuntimeException("Internal Exception");
             }
         });
-        thread.setName("New Worker Thread");
-        thread.setPriority(Thread.MAX_PRIORITY);
-        System.out.println("We are in thread: " + Thread.currentThread().getName() + " before starting a new thread");
-        thread.start();
-        System.out.println("We are in thread: " + Thread.currentThread().getName() + " after starting a new thread");
 
-        thread.sleep(10000);
+	    thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("A critical error happened in thread: " + t.getName() + " the error is " + e.getMessage());
+            }
+        });
+	    thread.start();
     }
 }
