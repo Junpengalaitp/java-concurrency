3c7fa362ab53ed4d24e5ad19593569f8eb6d18d6
Junpeng He <hejunpeng2012@hotmail.com>
2020-07-11 18:39:03 +0800

    CountDownLatch

---
+++ src/com/alaitp/countdownLatch/JoinCountDownLatch.java
@@ -0,0 +1,41 @@
+package com.alaitp.countdownLatch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class JoinCountDownLatch {
+
+    private static volatile CountDownLatch countDownLatch = new CountDownLatch(2);
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread threadOne = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                System.out.println("child thread one over!");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                countDownLatch.countDown();
+            }
+        });
+
+        Thread threadTwo = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+                System.out.println("child thread two over!");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                countDownLatch.countDown();
+            }
+        });
+
+        threadOne.start();
+        threadTwo.start();
+
+        System.out.println("wait all children thread finish!");
+
+        countDownLatch.await();
+
+        System.out.println("all children thread finished!");
+    }
+}
---
+++ src/com/alaitp/countdownLatch/JoinCountDownLatchPool.java
@@ -0,0 +1,34 @@
+package com.alaitp.countdownLatch;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class JoinCountDownLatchPool {
+    private static int threadNum = 10000;
+    private static volatile CountDownLatch countDownLatch = new CountDownLatch(threadNum);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < threadNum; i++) {
+            int finalI = i;
+            Thread thread = new Thread(() -> {
+                try {
+                    Thread.sleep(100);
+                    System.out.println("child thread " + finalI + " over!");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+            executorService.submit(thread);
+        }
+
+        System.out.println("wait all children thread finish!");
+
+        countDownLatch.await();
+
+        System.out.println("all children thread finished!");
+    }
+}
